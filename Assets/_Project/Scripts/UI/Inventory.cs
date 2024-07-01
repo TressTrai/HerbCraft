71c0cc4cb58eaea1150b025d1bd825b3ed92f516
llnnly <llnnnlly@gmail.com>
2024-07-01 13:40:02 +0500

    fixed a little more

--- Assets/_Project/Scripts/UI/Inventory.cs
+++ Assets/_Project/Scripts/UI/Inventory.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -149,7 +148,8 @@ public class Inventory : MonoBehaviour
     {
         for (int i = 0; i < items.Length; i++)
         {
-            RemoveItem(i);
+            items[i] = null;
+            cellsObjects[i] = null;
         }
     }
     public bool RemoveItem(int itemId)
