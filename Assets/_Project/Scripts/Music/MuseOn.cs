8c595d50421a23f596baf5277a36d7a4d26feda8
Misha <2004mif@gmail.com>
2024-07-01 11:59:32 +0500

    Music

--- Assets/_Project/Scripts/Music/MuseOn.cs
+++ Assets/_Project/Scripts/Music/MuseOn.cs
@@ -2,23 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quit : Button
+public class MuseOn : MonoBehaviour
 {
     public MusicPlayer audioSource;
 
     void Start()
     {
         audioSource.PlayMusic();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
     }
 }
