ef4432f48b8102115dbcef5a8a84f937385ca811
Misha <2004mif@gmail.com>
2024-08-06 23:16:07 +0500

    AudioManager added

---
+++ Assets/_Project/Scripts/Music/MusicZone.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicZone : MonoBehaviour
+{
+    private MusicPlayer musicPlayer;
+    private Movement movement;
+    public int changedMusic;
+    public int changeTo;
+    public int walkSound;
+
+    void Start()
+    {
+        musicPlayer = GameObject.FindGameObjectWithTag("MusicPlayer").GetComponent<MusicPlayer>();
+        movement = GameObject.FindGameObjectWithTag("PlayerBody").GetComponent<Movement>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerBody"))
+        {
+            musicPlayer.PauseMusic(changedMusic);
+            musicPlayer.PauseMusic(movement.walkSound);
+            if (musicPlayer.Time(changeTo) != 0)
+            {
+                musicPlayer.UnPauseMusic(changeTo);
+                movement.walkSound = walkSound;
+            }
+            else
+            {
+                musicPlayer.PlayMusic(changeTo);
+                musicPlayer.PlayMusic(walkSound);
+                movement.walkSound = walkSound;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerBody"))
+        {
+            musicPlayer.PauseMusic(changeTo);
+            musicPlayer.UnPauseMusic(changedMusic);
+            musicPlayer.PauseMusic(walkSound);
+            movement.walkSound = 3;
+        }
+    }
+}
